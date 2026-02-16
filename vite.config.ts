1409b4ae127ec349ee2d343889b7e822d517cfc0
Abdelrahman Balbaa <balbaa090@gmail.com>
2026-02-16 20:47:43 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "Abdelrahman-balbaa-portfolio",
+  base: "/Abdelrahman-balbaa-portfolio/",
   server: {
     host: "::",
     port: 8080,
